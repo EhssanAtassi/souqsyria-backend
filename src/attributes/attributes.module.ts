d307173ae54e8f8ff6e7e3c54c633b9a22bf5701
Ehsan Alatassi <ihssan.alatassi@gmail.com>
2025-06-08 12:40:12 +0300

    add all data to

--- src/attributes/attributes.module.ts
+++ src/attributes/attributes.module.ts
@@ -3,17 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Attribute } from './entities/attribute.entity';
 import { AttributeValue } from './entities/attribute-value.entity';
 import { AttributesService } from './attributes.service';
+import { AttributeValuesService } from './services/attribute-values.service';
 import { AttributesController } from './attributes.controller';
 import { User } from '../users/entities/user.entity';
 import { Route } from '../access-control/entities/route.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([
-    Attribute, AttributeValue,
-  User,Route
-  ])],
+  imports: [TypeOrmModule.forFeature([Attribute, AttributeValue, User, Route])],
   controllers: [AttributesController],
-  providers: [AttributesService],
-  exports: [TypeOrmModule],
+  providers: [AttributesService, AttributeValuesService],
+  exports: [TypeOrmModule, AttributesService, AttributeValuesService],
 })
 export class AttributesModule {}
